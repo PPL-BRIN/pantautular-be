5490456ac60a5419ce3727c859aaf81bb20b621c
alvaro.luqman1 <alvaro.luqman@gmail.com>
2025-02-13 11:35:22 +0700

    Fix attempt on SonarQube issue

--- pantau_tular/settings.py
+++ pantau_tular/settings.py
@@ -12,13 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#load_dotenv()
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
