a5e4d10906904a493b041b95e47aace4088212d0
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2025-03-19 21:28:42 +0700

    [REFACTOR] Add serializer for case age distribution

--- pt_backend/serializers.py
+++ pt_backend/serializers.py
@@ -6,3 +6,8 @@ class CaseLocationSerializer(serializers.Serializer):
     location__latitude = serializers.DecimalField(max_digits=8, decimal_places=6)
     city = serializers.CharField(max_length=255)
 
+class AgeDistributionSerializer(serializers.Serializer):
+    under_12 = serializers.IntegerField()
+    age_12_25 = serializers.IntegerField()
+    age_26_45 = serializers.IntegerField()
+    above_45 = serializers.IntegerField()
