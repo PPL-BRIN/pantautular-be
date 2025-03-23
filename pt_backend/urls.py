02d4c397b929f26b54e2514696162dc3b3ecabdf
yodh4 <cyrilus2004@gmail.com>
2025-03-23 21:40:58 +0700

    [RED] Create test for location severity url

--- pt_backend/urls.py
+++ pt_backend/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import AllCaseLocationsView, FiltersView, DiseaseSeverityStatsView
+from .views import (
+    AllCaseLocationsView, FiltersView, DiseaseSeverityStatsView,
+    LocationSeverityStatsView
+)
 
 urlpatterns = [
     path('cases/locations/', AllCaseLocationsView.as_view(), name='all-case-locations'),
     path('api/filters/', FiltersView.as_view(), name='filters'),
     path('api/diseases/severity-stats/', DiseaseSeverityStatsView.as_view(), name='disease-severity-stats'),
+    path('api/locations/severity-stats/')
 ]
