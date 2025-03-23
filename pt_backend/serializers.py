c7f9111cf0a31bdb42115c207cfa17c4719a18de
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2025-03-23 16:59:01 +0700

    [REFACTOR] Add TopPortalSerializer to handle portal data

--- pt_backend/serializers.py
+++ pt_backend/serializers.py
@@ -6,3 +6,6 @@ class CaseLocationSerializer(serializers.Serializer):
     location__latitude = serializers.DecimalField(max_digits=8, decimal_places=6)
     city = serializers.CharField(max_length=255)
 
+class TopPortalSerializer(serializers.Serializer):
+    portal = serializers.CharField()
+    count = serializers.IntegerField()
