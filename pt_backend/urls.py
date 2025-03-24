e0672bcf5f0ef7b8e4ac14aa36c1324860ae7bfb
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2025-03-24 23:24:08 +0700

    [GREEN] Add URL routes for TopLocalPortalsView and LocalPortalStatisticsView

--- pt_backend/urls.py
+++ pt_backend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AllCaseLocationsView, FiltersView
+from .views import AllCaseLocationsView, FiltersView, TopLocalPortalsView, LocalPortalStatisticsView
 
 urlpatterns = [
     path('cases/locations/', AllCaseLocationsView.as_view(), name='all-case-locations'),
     path('api/filters/', FiltersView.as_view(), name='filters'),
-]
+    path('api/dashboard/top-local-portals/', TopLocalPortalsView.as_view(), name='top-local-portals'),
+    path('api/dashboard/local-portal-stats/', LocalPortalStatisticsView.as_view(), name='local-portal-stats'),]
